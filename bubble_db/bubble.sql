4383249a86c6a1b1e6b777ab9f8fdc4b9e60c797
Robert Gama <31262157+gamarobert@users.noreply.github.com>
2018-04-04 13:02:45 -0700

    Added .sql file with user and group tables

---
+++ bubble_db/bubble.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE bubbleDB;
+
+USE bubbleDB;
+
+CREATE TABLE bUser (
+idUsername 			VARCHAR(16) 		NOT NULL 		PRIMARY KEY,
+userFullname 			VARCHAR(45) 		NOT NULL,
+userEmail 				VARCHAR(255) 		NOT NULL,
+userPassword 		VARCHAR(32) 		NOT NULL,
+userMajor				VARCHAR(45),
+user_create_time	TIMESTAMP			DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE bGroup (
+idGroup 							INT(16) 						NOT NULL 		PRIMARY KEY,
+groupCreator					VARCHAR(45)			NOT NULL,
+groupMajor 						VARCHAR(45) 			NOT NULL,
+groupSubjectClass 			VARCHAR(255) 			NOT NULL,
+groupNumParticipants 	INT(45)		 				NOT NULL,
+isPrivate							BINARY 						NOT NULL, #isPrivate: Yes or No
+isFull								BINARY 						NOT NULL, #isFull: Yes or No
+user_create_time			TIMESTAMP				DEFAULT CURRENT_TIMESTAMP
+);
+
+/*
+TESTING CODE
+------------------------------
+#INSERTS IN TABLE
+(idUsername, userFullname, userEmail, userPassword, userMAjor) 
+VALUES
+("username", "Robert Gama","email@gmail.com",  "Password", "Computer Science");
+
+SHOW tables; #SHOWS TABLES CREATED
+
+select * from bUser; #SHOWS DATA IN TABLE
+
+TRUNCATE TABLE bUser; #DELETES ALL DATA IN TABLE
+*/

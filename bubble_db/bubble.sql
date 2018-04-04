b75987ec3b5e9c6c15fcce4c1df18edb65d9fd69
Robert Gama <31262157+gamarobert@users.noreply.github.com>
2018-04-04 13:08:05 -0700

    Update bubble.sql

--- bubble_db/bubble.sql
+++ bubble_db/bubble.sql
@@ -3,24 +3,24 @@ CREATE DATABASE bubbleDB;
 USE bubbleDB;
 
 CREATE TABLE bUser (
-idUsername        VARCHAR(16) 		NOT NULL 		PRIMARY KEY,
-userFullname 	    VARCHAR(45) 		NOT NULL,
-userEmail 			  VARCHAR(255) 		NOT NULL,
-userPassword 		  VARCHAR(32) 		NOT NULL,
-userMajor				  VARCHAR(45),
-user_create_time	TIMESTAMP		  	DEFAULT CURRENT_TIMESTAMP
+idUsername VARCHAR(16) NOT NULL PRIMARY KEY,
+userFullname VARCHAR(45) NOT NULL,
+userEmail VARCHAR(255) NOT NULL,
+userPassword VARCHAR(32) NOT NULL,
+userMajor VARCHAR(45),
+user_create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 
 CREATE TABLE bGroup (
-idGroup 							  INT(16) 			  	NOT NULL 		PRIMARY KEY,
-groupCreator					  VARCHAR(45)	    	NOT NULL,
-groupMajor 						  VARCHAR(45) 	  	NOT NULL,
-groupSubjectClass 			VARCHAR(255) 			NOT NULL,
-groupNumParticipants  	INT(45)		 				NOT NULL,
-isPrivate						  	BINARY 						NOT NULL, #isPrivate: Yes or No
-isFull							  	BINARY 						NOT NULL, #isFull: Yes or No
-group_create_time		  	TIMESTAMP				  DEFAULT CURRENT_TIMESTAMP
+idGroup INT(16) NOT NULL PRIMARY KEY,
+groupCreator VARCHAR(45) NOT NULL,
+groupMajor VARCHAR(45) NOT NULL,
+groupSubjectClass VARCHAR(255) NOT NULL,
+groupNumParticipants INT(45) NOT NULL,
+isPrivate BINARY NOT NULL, #isPrivate: Yes or No
+isFull BINARY NOT NULL, #isFull: Yes or No
+group_create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 /*

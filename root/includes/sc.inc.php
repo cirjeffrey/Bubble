7f68ad6a22fc19d9953ef226d4facc353bc34080
Robert Gama <31262157+gamarobert@users.noreply.github.com>
2018-05-05 16:43:56 -0700

    now can click on creator of group and leads to profile

--- root/includes/sc.inc.php
+++ root/includes/sc.inc.php
@@ -57,7 +57,7 @@ include "dbh.inc.php";
 			
 				<tr>
 					<td> ".$row['groupSubjectClass']." </td>
-					<td> <a href='#'>".$name."</a> </td>
+					<td> <a href='./viewprofile.php?username=".$userName['idUsername']."'>".$name."</a> </td>
 					<td> ".$row['groupNumParticipants']." </td>
 					<form action = 'viewgroup.php?gid=$gid' method = 'POST'><td> <button name = 'join' type = 'submit' value = '$gid'>VIEW</button> </td></form>
 					<form action = 'includes/join.inc.php' method = 'POST'><td> <button name = 'join' type = 'submit' value = '$gid'>JOIN</button> </td></form>

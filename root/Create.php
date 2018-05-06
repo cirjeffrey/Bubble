1e9a354275cb24d178ad8505cd91a7342d2dfb47
catsc <32441749+catsc@users.noreply.github.com>
2018-05-05 18:48:42 -0700

    Updatre Create to look nice

--- root/Create.php
+++ root/Create.php
@@ -13,43 +13,35 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Made with Thimble</title>
-    <link rel="stylesheet" href="style.css">
-    <style>
-      .dropdown {
-        position: relative;
-        display: inline-block;
-        float: right;
-      }
-      .dropdown-content {
-        display: none;
-        position: absolute;
-        background-color: #f9f9f9;
-        min-width: 50px;
-        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-        padding: 12px 16px;
-      }
-      .dropdown:hover .dropdown-content {
-        display: block;
-      }
-    </style>
+    <link rel="stylesheet" href="create.css">
+    <link rel="stylesheet" href="navigator.css">
+    
   </head>
   <body>
     <header>
     <h1> LOGO</h1>  
     </header>
 
-    <div class="dropdown">
-      
-      <?php
-        $uid = $_SESSION['u_id'];
-        echo "<span>$uid</span>";
-      ?>
-      <div class="dropdown-content">
-        <a href="profile.php">My Profile</a>
-        <p>My Group</p>
-        <a href="includes/logout.inc.php?signout=true">Sign Out</a>
-      </div>
-    </div>
+    <!--Copy this into index, create, find, forum, and maybe profile-->
+<ul>
+        <li><a class="active" href="index.php">Home</a></li>
+        <li><a href="forum.php">Bulletin Board</a></li>
+        <li><a href="FindSG.php">Find Group</a></li>
+        <li><a href="#editGroup">Edit My Group</a></li>
+        <li><a href="#about">About</a></li>
+        <div class="dropdown">
+            <?php
+            $uid = $_SESSION['u_id'];
+            echo "<a href='profile.php' class='dropbtn'>$uid</a>";
+            ?>
+            <div class="dropdown-content">
+                <a href="profile.php">My Profile</a>
+                <a href="#editProfile">Edit My Profile</a>
+                <a href="includes/logout.inc.php?signout=true">Log out</a>
+            </div>
+        </div>
+    </ul>
+<!--____________________________________________________________-->
 
   <main>  
     <table>

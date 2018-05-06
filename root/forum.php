c15fd5cc5b7823d236290ce09d2e07e1557e0934
abraca-dabra <31224133+abraca-dabra@users.noreply.github.com>
2018-05-06 11:44:14 -0700

    Add navigator to all these forum pages

--- root/forum.php
+++ root/forum.php
@@ -1,36 +1,62 @@
-<?php	
-	session_start();
-	include ('content_function.php');
-	
-	if(!isset($_SESSION['u_id'])){
-	header("Location: ./login.html?please_log_in");
-	exit();
- }
-	
-?>
+<?php
+    session_start();
+    include ('content_function.php');
+    
+    if(!isset($_SESSION['u_id'])){
+        header("Location: ./login.html?please_log_in");
+        exit();
+    }
+    
+    ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<link rel="stylesheet" href="forum.css" type="text/css" />
-	<title>Bulletin Board</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="forum.css" type="text/css" />
+<link rel="stylesheet" href="navigator.css" type="text/css" />
+<title>Bulletin Board</title>
 </head>
 <body>
 
-	<header><h1><a href="./forum.php"> Bubbles Bulletin Board </a></h1></header>
-		<div>
-			<?php display_topics();?>
-		</div>
+<header>
+<h1> LOGO</h1>
+</header>
 
-	<?php
-		$uid = $_SESSION['u_id'];
-		if(isset($_SESSION['u_id']))
-		{
-			echo "<div><h4><a href='newtopic.php?'> Add a New Topic </a></h4></div>";
-		}
-	?>
-	
+<ul>
+<li><a class="active" href="index.php">Home</a></li>
+<li><a href="forum.php">Bulletin Board</a></li>
+<li><a href="FindSG.php">Find Group</a></li>
+<li><a href="#editGroup">Edit My Group</a></li>
+<li><a href="#about">About</a></li>
+<div class="dropdown">
+<?php
+    $uid = $_SESSION['u_id'];
+    echo "<a href='profile.php' class='dropbtn'>$uid</a>";
+    ?>
+<div class="dropdown-content">
+<a href="profile.php">My Profile</a>
+<a href="#editProfile">Edit My Profile</a>
+<a href="includes/logout.inc.php?signout=true">Log out</a>
+</div>
+</div>
+</ul>
+
+
+
+<header><h1>Bubbles Bulletin Board</h1></header>
+<div>
+<?php display_topics();?>
+</div>
+
+<?php
+    $uid = $_SESSION['u_id'];
+    if(isset($_SESSION['u_id']))
+    {
+        echo "<div><h4><a href='newtopic.php?'> Add a New Topic </a></h4></div>";
+    }
+    ?>
+
 </body>
 
 </html>
--- root/replyto.php
+++ root/replyto.php
@@ -11,9 +11,33 @@
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="forum.css" type="text/css" />
 	<title>Welcome to the Bubbles Bulletin Board</title></head>
-
+<link rel="stylesheet" href="navigator.css" type="text/css" />
 <body>
-	<div><h1><a href="./forum.php"> Bubbles Bulletin Board </a></h1></div>
+
+<header>
+<h1> LOGO</h1>
+</header>
+
+<ul>
+<li><a class="active" href="index.php">Home</a></li>
+<li><a href="forum.php">Bulletin Board</a></li>
+<li><a href="FindSG.php">Find Group</a></li>
+<li><a href="#editGroup">Edit My Group</a></li>
+<li><a href="#about">About</a></li>
+<div class="dropdown">
+<?php
+    $uid = $_SESSION['u_id'];
+    echo "<a href='profile.php' class='dropbtn'>$uid</a>";
+    ?>
+<div class="dropdown-content">
+<a href="profile.php">My Profile</a>
+<a href="#editProfile">Edit My Profile</a>
+<a href="includes/logout.inc.php?signout=true">Log out</a>
+</div>
+</div>
+</ul>
+
+	<div><h1>Bubbles Bulletin Board</h1></div>
 	
 	<div>
 	<?php

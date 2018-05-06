62ae6550c070e2ae1a8e37aa2b47c0231cb5f0a6
abraca-dabra <31224133+abraca-dabra@users.noreply.github.com>
2018-05-06 14:28:38 -0700

    Update about.php

--- root/about.php
+++ root/about.php
@@ -1,44 +1,49 @@
 <?php
-session_start();
-//redirect to login page if not logged in yet
-if(!isset($_SESSION['u_id'])){
-header("Location: ./login.html?pleaseLogIn");
-exit();
-}
+    session_start();
+    //redirect to login page if not logged in yet
+    if(!isset($_SESSION['u_id'])){
+        header("Location: ./login.html?pleaseLogIn");
+        exit();
+    }
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>About Us</title>
-<link rel="stylesheet" href="style.css">
-</head>
-<body>
+    <head>
+        <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+                <title>About Us</title>
+<link rel="stylesheet" href="forum.css" type="text/css" />
+<link rel="stylesheet" href="navigator.css" type="text/css" />
+    </head>
+    <body>
 
-<ul>
-<li><a class="active" href="index.php">Home</a></li>
-<li><a href="forum.php">Bulletin Board</a></li>
-<li><a href="FindSG.php">Find Group</a></li>
-<li><a href="#editGroup">Edit My Group</a></li>
-<li><a href="about.php">About</a></li>
-<div class="dropdown">
-<?php
-    $uid = $_SESSION['u_id'];
-    echo "<a href='profile.php' class='dropbtn'>$uid</a>";
-    ?>
-<div class="dropdown-content">
-<a href="profile.php">My Profile</a>
-<a href="#editProfile">Edit My Profile</a>
-<a href="includes/logout.inc.php?signout=true">Log out</a>
-</div>
-</div>
-</ul>
+<header>
+<h1> LOGO</h1>
+</header>
 
-<h1>About Us</h1>
-<p>
-We are a 6 people group called LFG 362, we are aim to solve your study problem.
-</p>
-</body>
+        <ul>
+            <li><a class="active" href="index.php">Home</a></li>
+            <li><a href="forum.php">Bulletin Board</a></li>
+            <li><a href="FindSG.php">Find Group</a></li>
+            <li><a href="#editGroup">Edit My Group</a></li>
+            <li><a href="about.php">About</a></li>
+            <div class="dropdown">
+                <?php
+                    $uid = $_SESSION['u_id'];
+                    echo "<a href='profile.php' class='dropbtn'>$uid</a>";
+                    ?>
+                <div class="dropdown-content">
+                    <a href="profile.php">My Profile</a>
+                    <a href="#editProfile">Edit My Profile</a>
+                    <a href="includes/logout.inc.php?signout=true">Log out</a>
+                </div>
+            </div>
+        </ul>
+        
+        <h1>About Us</h1>
+        <p>
+        We are a 6 people group called LFG 362, we are aim to solve your study problem.
+        </p>
+    </body>
 </html>

749f4c04277c960c30edeea732c97b42a80f4ea7
cirjeffrey <cirjeffrey92@gmail.com>
2018-05-05 20:18:48 -0700

    changed papges
    added titles
    
    added style to view group and homepage

--- root/Create.php
+++ root/Create.php
@@ -12,7 +12,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Made with Thimble</title>
+    <title>Create Group</title>
     <link rel="stylesheet" href="create.css">
     <link rel="stylesheet" href="navigator.css">
     
--- root/index.php
+++ root/index.php
@@ -10,63 +10,23 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Home</title>
+    <title>Homepage</title>
     <meta charset="utf-8"/>
-    <link rel="stylesheet" href="style.css" type="text/css" />
+    <link rel="stylesheet" href="homepage.css" type="text/css" />
     <!--<meta name="viewport" content="width=device-width, initial-scale=1"/> Commented this out and nothing changes, if not needed: delete-->
     
-    <!-- Keeping this here for reference for css team delete if not needed
-    <style>
-      .dropdown {
-        position: relative;
-        display: inline-block;
-        float:right;
-      }
-      .dropdown-content {
-        display: none;
-        position: absolute;
-        background-color: #f9f9f9;
-        min-width: 50px;
-        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-        padding: 12px 16px;
-      }
-      .dropdown:hover .dropdown-content {
-        display: block;
-      }
-    </style>
-    -->
+   
   </head>
   <body>
     <header>
-      <h1> logo</h1>
+      <h1> Welcome</h1>
     </header>
     
     
 
-    <!--Copy this into index, create, find, forum, and maybe profile-->
-    <ul>
-        <li><a class="active" href="index.php">Home</a></li>
-        <li><a href="forum.php">Bulletin Board</a></li>
-        <li><a href="FindSG.php">Find Group</a></li>
-        <li><a href="#editGroup">Edit My Group</a></li>
-        <li><a href="#about">About</a></li>
-        <div style="float:right" class="dropdown">
-            <?php
-            $uid = $_SESSION['u_id'];
-            echo "<a href='profile.php' class='dropbtn'>$uid</a>";
-            ?>
-            <div class="dropdown-content">
-                <a href="profile.php">My Profile</a>
-                <a href="#editProfile">Edit My Profile</a>
-                <a href="includes/logout.inc.php?signout=true">Log out</a>
-            </div>
-        </div>
-    </ul>
-
     <main>
       <form>
         <input type="button" value="Find Study Group" onclick="window.location.href='FindSG.php'" />
-        
         
         <input type="button" value="Create Study Group" onclick="window.location.href='Create.php'" />
         

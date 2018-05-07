12e063d8b452d7619995067af1ca9f6876e4a052
Sarah Nuno <saritanu321@yahoo.com>
2018-05-06 22:17:34 -0700

    got rid of edit my group

--- root/forum.php
+++ root/forum.php
@@ -12,10 +12,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
-<link rel="stylesheet" href="forum.css" type="text/css" />
-<link rel="stylesheet" href="navigator.css" type="text/css" />
-<title>Bulletin Board</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="forum.css" type="text/css" />
+    <link rel="stylesheet" href="navigator.css" type="text/css" />
+    <title>Bulletin Board</title>
 </head>
 <body>
 
@@ -24,20 +24,18 @@
 </header>
 
 <ul>
-<li><a class="active" href="index.php">Home</a></li>
-<li><a href="forum.php">Bulletin Board</a></li>
-<li><a href="FindSG.php">Find Group</a></li>
-<li><a href="#editGroup">Edit My Group</a></li>
-<li><a href="about.php">About</a></li>
+    <li><a class="active" href="index.php">Home</a></li>
+    <li><a href="forum.php">Bulletin Board</a></li>
+    <li><a href="FindSG.php">Find Group</a></li>
+    <li><a href="about.php">About</a></li>
 <div class="dropdown">
 <?php
     $uid = $_SESSION['u_id'];
     echo "<a href='profile.php' class='dropbtn'>$uid</a>";
     ?>
 <div class="dropdown-content">
-<a href="profile.php">My Profile</a>
-<a href="#editProfile">Edit My Profile</a>
-<a href="includes/logout.inc.php?signout=true">Log out</a>
+    <a href="profile.php">My Profile</a>
+    <a href="includes/logout.inc.php?signout=true">Log out</a>
 </div>
 </div>
 </ul>
